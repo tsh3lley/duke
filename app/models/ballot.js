36c02fdaf6992727e124104392ea069720a3fa44
the kid <thomasjshelley@gmail.com>
2016-11-19 21:08:40 -0500

    ITS ALIVE

--- app/models/ballot.js
+++ app/models/ballot.js
@@ -1,7 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BallotSchema   = new Schema({
+var BallotSchema = new Schema({
     phone: Number,
     vote1: String,
     vote2: String,
